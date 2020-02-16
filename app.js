e8f82b6609e2e04ead8274f5da5965477794ae91
drabick <drabick@hawk.iit.edu>
2020-02-16 16:09:13 -0600

    Setup index html to load main view at start

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars');
 
 const app = express();
 
+//Set Route folders for all gets and Posts
 app.use('/gets', getsRoute);
 app.use('/posts', postsRoute);
 
@@ -13,13 +14,10 @@ app.engine('handlebars', handlebars({
     }));
 app.set('view engine', 'handlebars');
 
-
-
-
-
-//routes
+//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
 app.get('/', (req,res) => {
-    res.send('Chicago Crime Analyst Team Project');    
+    //res.render('main', {crimes: cdata});
+    res.render('main', {layout : 'index'});   
 });
 
 // lisgerner
