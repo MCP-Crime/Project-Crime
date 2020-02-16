ab12accc0a81c9e5a7cd87cc0f0f75cefb4ebe8f
drabick <drabick@hawk.iit.edu>
2020-02-15 18:16:21 -0600

    Added core express requirement route and listener to app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express = require('express');
 
+const app = express();
+
+//routes
+app.get('/', (req,res) => {
+    res.send('Chicago Crime Analyst Team Project');    
+});
+
+// lisgerner
+app.listen(3000);
