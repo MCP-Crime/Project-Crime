f7676ae317f99a0d435e8caa57ba4195b642ec27
drabick <drabick@hawk.iit.edu>
2020-02-16 15:48:37 -0600

    Added routes designators and set template engine for handlebars.

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
+
+app.use('/gets', getsRoute);
+app.use('/posts', postsRoute);
+
+//Handlebar engine
+//Sets handlebars configurations
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',defaultLayout: 'index',
+    }));
+app.set('view engine', 'handlebars');
+
+
+
+
 
 //routes
 app.get('/', (req,res) => {
