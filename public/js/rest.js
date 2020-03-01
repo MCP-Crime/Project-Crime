a84928968425bc659268c39bf6f03d019a006539
shruthi-bv <sbhavaraju@hawk.iit.edu>
2020-02-29 19:31:04 -0600

    added routes and models

--- public/js/rest.js
+++ public/js/rest.js
@@ -1,6 +1,6 @@
 function getCrimesLatestData(callback){
     $.ajax({
-        url: "https://data.cityofchicago.org/resource/crimes.json",
+        url: "https://data.cityofchicago.org/resource/ijzp-q8t2.json", //"https://data.cityofchicago.org/resource/crimes.json",
         method: "GET",
         data: {
             "$limit" : 200,
@@ -14,3 +14,20 @@ function getCrimesLatestData(callback){
         }
     });
 }
+
+// function getCrimesLatestData(callback){
+//     $.ajax({
+//         url: "https://data.cityofchicago.org/resource/ijzp-q8t2.json?$where=block like" + " '%25" + '75TH' + "%25'",
+//         method: "GET",
+//         data: {
+//             "$limit" : 200,
+//             "$$app_token" : "4LSXHra0DVbTUwNPgC4whpMzH"
+//         },
+//         success: function (data) {
+//             callback(data);
+//         },
+//         error: function (err) {
+//             console.log(err);
+//         }
+//     });
+// }
