21de7401365c23038ad8ca941b092801b91669e9
Raghulkrishnan <rbalasubramanian1@hawk.iit.edu>
2020-04-03 21:56:26 -0500

    .

---
+++ public/models/streetModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const streetSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    date : {type : Date, required : true},
+    block_name_searched : {type : String, required : true},
+    block_name : {type : String, required : true},
+    location_description : {type : String, required : true},
+    timestamps : {type : Date, required : true}
+}
+);
+
+module.exports = mongoose.model('Street', streetSchema);
